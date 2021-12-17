e41bbaaa244aa986f3c26ea533441a01e9fa65aa
Shokhrukhmirzo Toyirov <toirovshohruxmirzo@gmail.com>
2021-12-17 21:12:03 +0500

    Custom font/Remix Icons added

--- src/Screens/Stack/Home/index.js
+++ src/Screens/Stack/Home/index.js
@@ -2,13 +2,45 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles';
 import RussiaSVG from '@assets/SVG/044-russia.svg';
-// import { Svg } from 'react-native-svg';
+import Icon from 'react-native-remix-icon';
+import LinearGradient from 'react-native-linear-gradient';
+import VectorIcon from 'react-native-vector-icons/Ionicons';
+import LottieView from 'lottie-react-native';
+
+const LoadingAnim = require('@assets/JSON/loadingNav.json');
 
 const Home = () => {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
-      <RussiaSVG width={50} height={50} />
+      <View style={styles.section}>
+        <Text>Custom SVG icons</Text>
+        <RussiaSVG width={50} height={50} />
+      </View>
+      <View style={styles.section}>
+        <Icon name="map-pin-range-line" size="48" color="#6666ff" />
+        <Text>Remix Icons</Text>
+      </View>
+      <View style={styles.section}>
+        <VectorIcon name="ios-heart" size={48} color="#ff0000" />
+        <Text>Vector Icons</Text>
+      </View>
+      <View style={styles.section}>
+        <Text style={styles.customFont}>Custom Font Family: Ubuntu</Text>
+      </View>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Lottie Animations</Text>
+        <LottieView source={LoadingAnim} autoPlay loop style={styles.animation} />
+      </View>
+      <View style={styles.section}>
+        <LinearGradient
+          style={styles.gradient}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          colors={['#0f0', '#f00']}
+        >
+          <Text style={{ color: '#fff' }}>Linear Gradient</Text>
+        </LinearGradient>
+      </View>
     </View>
   );
 };
--- src/Screens/Stack/Home/styles.js
+++ src/Screens/Stack/Home/styles.js
@@ -7,6 +7,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  section: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 20,
+  },
+  customFont: {
+    fontFamily: 'Ubuntu-Regular',
+    fontSize: 25,
+  },
+  gradient: {
+    padding: 20,
+    borderRadius: 5,
+    width: '80%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  animation: {
+    width: 200,
+    height: 200,
+  },
 });
 
 export default styles;
