93a3969258c6d54e3cb0ee6a7ea54d6a22b174c3
Shokhrukhmirzo Toyirov <toirovshohruxmirzo@gmail.com>
2021-12-25 00:13:46 +0500

    Map style url

--- src/Screens/Bottom/Navigation/index.js
+++ src/Screens/Bottom/Navigation/index.js
@@ -1,18 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, usesEffect, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
+import MapboxGL from '@react-native-mapbox-gl/maps';
+import RNLocation from 'react-native-location';
 import Container from '@components/Atoms/Container';
 import { COLORS as colors } from '@constants';
-import MapboxGL from '@react-native-mapbox-gl/maps';
 
 const Dashboard = () => {
-  const [coordinates] = useState([69.294861, 41.316441]);
+  const [coordinates, setCoordinates] = useState([69.294861, 41.316441]);
+
+  useEffect(() => {
+    const unsubscribe = RNLocation.subscribeToLocationUpdates((locations) => {
+      let latestLocation = { timestamp: 0 };
+      locations.forEach((location, index) => {
+        if (location.timestamp > latestLocation.timestamp) {
+          latestLocation = location;
+        }
+      });
+      setCoordinates([latestLocation.longitude, latestLocation.latitude]);
+    });
+    return unsubscribe;
+  }, []);
 
   return (
     <Container style={styles.container}>
-      <MapboxGL.MapView style={styles.map} zoomEnabled={true}>
+      <MapboxGL.MapView
+        style={styles.map}
+        zoomEnabled={true}
+        styleURL="mapbox://styles/polaris-maps/ckxkpuan6lmpk15o5is31ipm7"
+      >
         <MapboxGL.UserLocation />
-        <MapboxGL.Camera zoomLevel={4} centerCoordinate={coordinates} />
+        <MapboxGL.Camera zoomLevel={13} centerCoordinate={coordinates} />
         <MapboxGL.PointAnnotation coordinate={coordinates} id={'asd'} />
       </MapboxGL.MapView>
     </Container>
