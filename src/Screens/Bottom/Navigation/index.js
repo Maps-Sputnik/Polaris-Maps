c129379db49bb23aeda3d65fe7edac788b2f27d7
Shokhrukhmirzo Toyirov <toirovshohruxmirzo@gmail.com>
2021-12-25 15:19:57 +0500

    Mapview removed

--- src/Screens/Bottom/Navigation/index.js
+++ src/Screens/Bottom/Navigation/index.js
@@ -2,29 +2,9 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import Container from '@components/Atoms/Container';
 import { COLORS as colors } from '@constants';
-import MapView, { Marker } from 'react-native-maps';
 
 const Dashboard = ({ navigation }) => {
-  return (
-    <Container style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 40.374132,
-          longitude: 71.472253,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        <Marker
-          coordinate={{
-            latitude: 40.374132,
-            longitude: 71.472253,
-          }}
-        />
-      </MapView>
-    </Container>
-  );
+  return <Container style={styles.container}></Container>;
 };
 
 export default Dashboard;
