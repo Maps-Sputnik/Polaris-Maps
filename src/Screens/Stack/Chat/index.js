c04a1ae9b4ff3ddb966a0da9eab22284baac66e7
bakhr0mkhan <softdeveloperuz@gmail.com>
2021-12-26 14:46:30 +0500

    Done chat screen

--- src/Screens/Stack/Chat/index.js
+++ src/Screens/Stack/Chat/index.js
@@ -1,24 +1,23 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { Text, View, Dimensions, Linking, StyleSheet } from 'react-native';
+import { Text, View, Linking, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { GiftedChat } from 'react-native-gifted-chat';
+import { GiftedChat, Send, Day, Time, Bubble } from 'react-native-gifted-chat';
 import FastImage from 'react-native-fast-image';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import Entypo from 'react-native-vector-icons/Entypo';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
-import Container from '@components/Atoms/Container';
 import Touchable from '@components/Atoms/TouchableOpacity';
 import { SIZES as sizes, COLORS as colors, MAIN_HEADER } from '@constants';
-// import styles from './styles';
+import styles from './styles';
 
 const Chat = () => {
+  // hooks
   const navigation = useNavigation();
   const insets = useSafeAreaInsets();
-
+  // states
   const [messages, setMessages] = useState([]);
-
+  // effects
   useEffect(() => {
     setMessages([
       {
@@ -33,13 +32,43 @@ const Chat = () => {
       },
     ]);
   }, []);
-
+  // callbacks
   const onSend = useCallback((messages = []) => {
     setMessages((previousMessages) => GiftedChat.append(previousMessages, messages));
   }, []);
 
   function handleCall() {
     Linking.openURL(`tel:+998999999999`);
+  }
+  function handlePopup() {
+    console.warn('Handled later...');
+  }
+
+  // function comps
+  function renderEmptyChat() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={styles.emptyChatTxt}>No messages here yet...</Text>
+      </View>
+    );
+  }
+  function customSendBtn(props) {
+    return (
+      <Send {...props}>
+        <Ionicons name="ios-send-sharp" size={sizes.icon} color="#018AEB" style={styles.sendBtn} />
+      </Send>
+    );
+  }
+  function renderDay(props) {
+    return <Day {...props} textStyle={styles.dayTxt} />;
+  }
+
+  function renderBubble(props) {
+    return <Bubble {...props} textStyle={styles.bubbleTxt} wrapperStyle={styles.bubbleWrapper} />;
+  }
+
+  function renderTime(props) {
+    return <Time {...props} textStyle={styles.timeTxt} />;
   }
   return (
     <>
@@ -58,63 +87,32 @@ const Chat = () => {
           <Touchable style={styles.btns} onPress={handleCall}>
             <FontAwesome5 name="phone-alt" size={sizes.icon * 0.8} color="black" />
           </Touchable>
-          <Touchable style={styles.btns}>
+          <Touchable style={styles.btns} onPress={handlePopup}>
             <Entypo name="dots-three-vertical" size={sizes.icon * 0.8} color="black" />
           </Touchable>
         </View>
       </View>
       <GiftedChat
         messages={messages}
+        renderChatEmpty={renderEmptyChat}
+        renderSend={customSendBtn}
+        textInputStyle={styles.textInput}
+        style={styles.chatCon}
         onSend={(messages) => onSend(messages)}
+        renderDay={renderDay}
+        renderBubble={renderBubble}
+        renderTime={renderTime}
         user={{
           _id: 1,
+        }}
+        listViewProps={{
+          style: {
+            backgroundColor: '#f8f9fa',
+          },
         }}
       />
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: (insets) => ({
-    backgroundColor: 'white',
-    flex: 1,
-    // paddingTop: MAIN_HEADER.height + insets.top + MAIN_HEADER.top + 10,
-  }),
-  avatarImg: {
-    width: 50,
-    height: 50,
-    borderRadius: 50,
-    resizeMode: 'cover',
-  },
-  headerCon: {
-    justifyContent: 'space-between',
-    borderBottomWidth: 0.5,
-    borderColor: '#b7b7a4',
-    paddingHorizontal: 5,
-    paddingVertical: 5,
-    backgroundColor: 'white',
-  },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  btns: {
-    marginHorizontal: 10,
-  },
-  textCon: {
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    paddingHorizontal: 10,
-  },
-  nameTxt: {
-    fontFamily: 'Lato-Bold',
-    fontSize: sizes.h4,
-    color: 'black',
-  },
-  statusTxt: {
-    fontFamily: 'Lato-Regular',
-    color: '#018AEB',
-  },
-});
 
 export default Chat;
--- src/Screens/Stack/Chat/styles.js
+++ src/Screens/Stack/Chat/styles.js
@@ -5,13 +5,89 @@ const styles = StyleSheet.create({
   container: (insets) => ({
     backgroundColor: 'white',
     flex: 1,
-    paddingTop: MAIN_HEADER.height + insets.top + MAIN_HEADER.top + 10,
+    // paddingTop: MAIN_HEADER.height + insets.top + MAIN_HEADER.top + 10,
   }),
   avatarImg: {
-    width: 60,
-    height: 60,
-    borderRadius: 60,
+    width: 50,
+    height: 50,
+    borderRadius: 50,
     resizeMode: 'cover',
+  },
+  headerCon: {
+    justifyContent: 'space-between',
+    borderBottomWidth: 0.5,
+    borderColor: '#b7b7a4',
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+    backgroundColor: 'white',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  btns: {
+    marginHorizontal: 10,
+  },
+  textCon: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+  },
+  nameTxt: {
+    fontFamily: 'Lato-Bold',
+    fontSize: sizes.h4,
+    color: 'black',
+  },
+  statusTxt: {
+    fontFamily: 'Lato-Regular',
+    color: '#018AEB',
+  },
+  chatCon: {
+    backgroundColor: 'red',
+  },
+  sendBtn: {
+    marginHorizontal: 10,
+  },
+  textInput: {
+    lineHeight: sizes.h4,
+    fontFamily: 'Lato-Regular',
+    fontSize: sizes.h4,
+  },
+  emptyChatTxt: {
+    transform: [{ scaleY: -1 }],
+    fontSize: sizes.h5,
+    fontFamily: 'Lato-Regular',
+  },
+  bubbleTxt: {
+    left: {
+      color: 'white',
+      fontFamily: 'Lato-Regular',
+    },
+    right: {
+      color: 'white',
+      fontFamily: 'Lato-Regular',
+    },
+  },
+  bubbleWrapper: {
+    left: {
+      backgroundColor: '#018AEB',
+    },
+    right: {
+      backgroundColor: '#018AEB',
+    },
+  },
+  dayTxt: {
+    color: colors.text,
+    fontSize: sizes.h6,
+    fontFamily: 'Lato-Regular',
+  },
+  timeTxt: {
+    right: {
+      color: 'white',
+    },
+    left: {
+      color: 'white',
+    },
   },
 });
 
