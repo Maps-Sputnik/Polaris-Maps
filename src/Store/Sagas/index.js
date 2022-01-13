b78ba8cf3e61aad1e0ee52ac8769badec9354d4b
Shokhrukhmirzo Toyirov <toirovshohruxmirzo@gmail.com>
2022-01-13 14:47:14 +0500

    fixed

--- src/Store/Sagas/index.js
+++ src/Store/Sagas/index.js
@@ -1,13 +1,13 @@
 import { takeLatest } from 'redux-saga/effects';
 import * as types from '../Actions/types';
-import UserSagas from './User';
+import User from './User';
 import Post from './Post';
 import Friend from './Friend';
 
 function* MainSaga() {
-  yield takeLatest(types.GET_CONTACTS, UserSagas.getContacts);
+  yield takeLatest(types.GET_CONTACTS, User.getContacts);
   yield takeLatest(types.FETCH_POSTS_REQUEST, Post.fetch);
-  yield takeLatest(types.MAKE_FRIEND_REQUEST, Post.makeFriend);
+  yield takeLatest(types.MAKE_FRIEND_REQUEST, Friend.makeFriend);
 }
 
 export default MainSaga;
